6f285aa0d1accc7992a87eaeb22db77b158fa2f7
abraa9063-beep <abraa9063@gmail.com>
2026-02-27 13:44:31 -0300

    Update firebase.js

--- src/scripts/firebase.js
+++ src/scripts/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBt86uVSewEe4-NKjGyZCEm4Qn_y1Ap1vM",
   authDomain: "relatorios-estoque.firebaseapp.com",
@@ -13,5 +11,9 @@ const firebaseConfig = {
   appId: "1:164779102499:web:717611e5dd0f2fc5977c96"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
